3aefe2d38ed98df2dc37c3492d2ed7ee95b1941d
Aleksandr Korotkikh <koralvas@gmail.com>
2019-07-09 10:48:34 +0600

    spec written

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -2,10 +2,89 @@ require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
   describe '#update_quality' do
-    it 'does not change the name' do
-      items = [Item.new('foo', 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq 'fixme'
+    before do
+      described_class.new([sample_item]).update_quality
+    end
+
+    context 'regular item' do
+      let(:sample_item) { Item.new('foo', 2, 10) }
+      it 'does not change the name' do
+        expect(sample_item.name).to eq 'foo'
+      end
+
+      context 'sell_in passes' do
+        let(:sample_item) { Item.new('foo', -1, 10) }
+        it 'decrease quality by 2' do
+          expect(sample_item.quality).to eq 8
+        end
+      end
+      context 'lowest quality' do
+        let(:sample_item) { Item.new('foo', 1, 0) }
+        it 'not change quality' do
+          expect(sample_item.quality).to eq 0
+        end
+      end
+      it 'decrease sell_in by 1' do
+        expect(sample_item.sell_in).to eq 1
+      end
+      it 'decrease quality by 1' do
+        expect(sample_item.quality).to eq 9
+      end
+    end
+
+    context 'Aged Brie item' do
+      let(:sample_item) { Item.new('Aged Brie', 2, 10) }
+      it 'increase quality' do
+        expect(sample_item.quality).to eq 11
+      end
+
+      context 'maximum quality' do
+        let(:sample_item) { Item.new('Aged Brie', 2, 50) }
+        it 'not change quality' do
+          expect(sample_item.quality).to eq 50
+        end
+      end
+    end
+
+    context 'Sulfuras item' do
+      let(:sample_item) { Item.new('Sulfuras, Hand of Ragnaros', 2, 80) }
+      it 'not change quality' do
+        expect(sample_item.quality).to eq 80
+      end
+      it 'not change sell_in' do
+        expect(sample_item.sell_in).to eq 2
+      end
+    end
+
+    context 'Backstage passes item' do
+      let(:sample_item) { Item.new('Backstage passes to a TAFKAL80ETC concert', days, 10) }
+      context 'more than 10 days left' do
+        let(:days) { 11 }
+        it 'increase quality by 1' do
+          expect(sample_item.quality).to eq 11
+        end
+      end
+
+      context '10 days left' do
+        let(:days) { 10 }
+        it 'increase quality by 2' do
+          expect(sample_item.quality).to eq 12
+        end
+      end
+
+      context '5 days left' do
+        let(:days) { 5 }
+        it 'increase quality by 3' do
+          expect(sample_item.quality).to eq 13
+        end
+      end
+
+      context 'concert passed' do
+        let(:days) { -1 }
+        it 'drop quality to 0' do
+          expect(sample_item.quality).to eq 0
+        end
+      end
     end
   end
 end
